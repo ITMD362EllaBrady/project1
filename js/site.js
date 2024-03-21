7df033a6503eaa42ecbd2addc64520dabb236a14
Ella Brady <112596525+ellaroseb@users.noreply.github.com>
2024-03-21 18:07:39 -0500

    moved brackets

--- js/site.js
+++ js/site.js
@@ -2,10 +2,10 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-  
+});  
+
   function ValidateEmail(email) {
   var re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
-});
